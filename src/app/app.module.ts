f5da76ba44a13def616ed77b2853ffb07876c41c
nydi <stefan.nydegger@gmail.com>
2018-01-14 17:30:17 +0100

    section 2 lecture 19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
